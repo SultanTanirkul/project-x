856007ef773a9cf7746be7c1ebdf22d6ec10a2d2
TheCub3 <33991163+sultantanirkul@users.noreply.github.com>
2019-03-22 01:01:34 +0000

    Added basic view and functionality for profile activity

--- app/src/main/java/com/example/group18/buddybuddygym/MainActivity.java
+++ app/src/main/java/com/example/group18/buddybuddygym/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.group18.buddybuddygym;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,7 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.HashMap;
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText m_Password_EditText = null;
     private Button   m_Login_Button      = null;
     private Button   m_LogOut_Button     = null;
+    private Button   m_Profile_Button    = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,43 +41,50 @@ public class MainActivity extends AppCompatActivity {
         m_RegistrationLink = findViewById(R.id.link_register);
         m_ForgetPassLink = findViewById(R.id.link_forget_pass);
         m_LogOut_Button = findViewById(R.id.sign_out_button);
+        m_Profile_Button = findViewById(R.id.profile_button);
 
         // Validation if user is signed in.
         checkIfLogged();
 
         // Some functionality for Login Button
         m_Login_Button.setOnClickListener((View v)->{
-                ServerOp serverOp = ServerOp.getInstance(getApplicationContext());
-                Map<String, String> userLoginParams = new HashMap<>();
+            ServerOp serverOp = ServerOp.getInstance(getApplicationContext());
+            Map<String, String> userLoginParams = new HashMap<>();
+            userLoginParams.put("username", m_Username_EditText.getText().toString());
+            userLoginParams.put("password", m_Password_EditText.getText().toString());
+            ProgressDialog dialog = ProgressDialog.show(MainActivity.this, "",
+                    "Loading. Please wait...", true);
+            dialog.show();
+            serverOp.addToRequestQueue(serverOp.postRequest("https://limberup.herokuapp.com/authenticate", userLoginParams ,(s) -> {
+                try {
+                    isSignIn(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }));
+            dialog.hide();
 
-                userLoginParams.put("username", m_Username_EditText.getText().toString());
-                userLoginParams.put("password", m_Password_EditText.getText().toString());
-
-                serverOp.addToRequestQueue(serverOp.postRequest("https://limberup.herokuapp.com/authenticate", userLoginParams ,(s) -> {
-                    try {
-                        isSignIn(s);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }));
-                m_Password_EditText.setText(null);
-                m_Username_EditText.setText(null);
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(m_Password_EditText.getWindowToken(), 0);
-                imm.hideSoftInputFromWindow(m_Username_EditText.getWindowToken(), 0);
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(m_Password_EditText.getWindowToken(), 0);
+            imm.hideSoftInputFromWindow(m_Username_EditText.getWindowToken(), 0);
         });
 
         // Some functionality for registration link/text.
         m_RegistrationLink.setOnClickListener((View v) ->
         {
-                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-                startActivity(intent);
-            });
+            Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+            startActivity(intent);
+        });
 
         // Some functionality for Log out button.
         m_LogOut_Button.setOnClickListener((View v) -> {
                     signOut();
-                });
+        });
+
+        m_Profile_Button.setOnClickListener((View v) -> {
+            Intent intent = new Intent(MainActivity.this, UserProfileActivity.class);
+            startActivity(intent);
+        });
     }
 
     /**
@@ -96,9 +105,14 @@ public class MainActivity extends AppCompatActivity {
             SharedPreferences.Editor editor = sharedPref.edit();
             editor.putString("username", m_Username_EditText.getText().toString());
             editor.putString("password", m_Password_EditText.getText().toString());
-            editor.commit();
+            editor.apply();
+
+            Log.v("ClientError", m_Username_EditText.getText().toString() + " " + m_Password_EditText.getText().toString());
+
             Log.v("Server", serverResponse);
             changeLayout(View.GONE);
+            m_Password_EditText.setText(null);
+            m_Username_EditText.setText(null);
             return true;
         }
         Log.v("Server: ", serverResponse);
@@ -124,6 +138,7 @@ public class MainActivity extends AppCompatActivity {
             Map<String, String> userLoginParams = new HashMap<>();
             userLoginParams.put("username", username);
             userLoginParams.put("password", password);
+            Log.v("Client", username + " " + password);
             serverOp.addToRequestQueue(serverOp.postRequest("https://limberup.herokuapp.com/authenticate", userLoginParams , (s) -> Log.v("Server", s)));
             changeLayout(View.GONE);
             Toast.makeText(this,"Thank you for using LimberUP", Toast.LENGTH_LONG).show();
@@ -143,9 +158,10 @@ public class MainActivity extends AppCompatActivity {
         m_ForgetPassLink.setVisibility(visibilityType);
         if(visibilityType == View.VISIBLE){
             m_LogOut_Button.setVisibility(View.GONE);
+            m_Profile_Button.setVisibility(View.GONE);
         }else{
             m_LogOut_Button.setVisibility(View.VISIBLE);
-
+            m_Profile_Button.setVisibility(View.VISIBLE);
         }
     }
 
@@ -156,10 +172,11 @@ public class MainActivity extends AppCompatActivity {
     private boolean signOut() {
         SharedPreferences sharedPref = MainActivity.this.getSharedPreferences(
                 getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        sharedPref.edit().remove("username").commit();
-        sharedPref.edit().remove("password").commit();
+        sharedPref.edit().remove("username").apply();
+        sharedPref.edit().remove("password").apply();
         changeLayout(View.VISIBLE);
         Toast.makeText(this,"Signed Out successfully", Toast.LENGTH_LONG).show();
+
         return true;
     }
 }
