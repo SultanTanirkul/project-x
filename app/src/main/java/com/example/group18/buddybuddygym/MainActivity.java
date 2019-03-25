bac4e80ab6236c093a6368054834e5a340a56199
Louis Syropoulo <ls00953@surrey.ac.uk>
2019-03-25 14:08:09 +0000

    louis

--- app/src/main/java/com/example/group18/buddybuddygym/MainActivity.java
+++ app/src/main/java/com/example/group18/buddybuddygym/MainActivity.java
@@ -6,17 +6,25 @@ import android.widget.TextView;
 
 import com.android.volley.toolbox.StringRequest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.server_test_page);
         TextView text = (TextView) findViewById(R.id.text);
         ServerOp serverOp = ServerOp.getInstance(getApplicationContext());
         text.setText("hello");
-        StringRequest newUser = serverOp.getRequest("http://10.0.2.2:9000/createUser", s -> text.setText(s));
-        serverOp.addToRequestQueue(newUser);
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("username", "newUser");
+        params.put("password", "helloworld");
+        StringRequest login = serverOp.postRequest("http://10.0.2.2:9000/createUser", params, s -> text.setText(s));
+        serverOp.addToRequestQueue(login);
+        StringRequest viewRestricted = serverOp.getRequest("http://10.0.2.2:9000/user", s -> text.setText(s));
+        serverOp.addToRequestQueue(viewRestricted);
 
 
     }
